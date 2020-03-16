f1317a4e79b3fc68ec1cddce853b8363bb31e3a8
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-03-16 12:24:05 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,37 +18,52 @@ print(len(pages))
 
 liste_lien=[]
 for page in pages:
-    print(page)
+    #print(page)
     link= page.get("href")
     liste_lien.append(link)
 
-print(liste_lien)
-print("nombre de page",len(liste_lien))
+leLien=liste_lien[len(liste_lien)-2]
+resteLien=""
+dernierNum=""
+passe=False
+for i in leLien:
+    if not passe :
+        resteLien=resteLien+i
+        print("i=",i)
+        if i== "=":
+            passe=True
+    else :
+        dernierNum=dernierNum+i
+
+print("coucou"+resteLien)
+print("Derniere page : "+dernierNum)
+
+ 
+print("c'est lui"+liste_lien[len(liste_lien)-2])
 
 print("nombre de restos",len(rows))
 
 print("Nom des restos :")
+listeResto=[]
 for i in rows:
     for j in i:
         for a in j:
+            listeResto.append(a)
+print(listeResto)
+#print(len(rows))
 
-            print(a, ",")
-
-print("c'est lui :     "+liste_lien[len(liste_lien)-2])
 
 
-#leLien=liste_lien[len(liste_lien)-2]
-#resteLien=""
-#dernierNum=""
-#passe=False
-#for i in leLien:
-#    if not passe :
-#        resteLien=resteLien+i
-#        print("i=",i)
-#        if i== "=":
-#            passe=True
-#    else :
-#        dernierNum=dernierNum+i
-#
-#print("coucou"+resteLien)
-#print("Derniere page : "+dernierNum)
+for i in range(2,int(dernierNum)+1,1):
+    html = urlopen("https://www.linternaute.com/%22+resteLien+str(i))
+    html_soup = BeautifulSoup(html, 'html.parser')
+    rows = html_soup.findAll("h2")
+    listeR=[]
+
+    print("Nom des restos :")
+    c=[]
+    for i in rows:
+        for j in i:
+            for a in j:
+                listeR.append(a)
+    print(listeR)
