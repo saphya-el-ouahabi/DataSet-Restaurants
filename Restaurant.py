62a6c87327408e577ff387faa308b8a3b99d0c74
saphya-el-ouahabi <56952563+saphya-el-ouahabi@users.noreply.github.com>
2020-05-12 15:07:06 +0200

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 16 08:32:44 2020
 
-@author: elsar
-"""
 " IMPORTATION  "
 #------------------------------------------------------------------------------
 from urllib.request import urlopen
